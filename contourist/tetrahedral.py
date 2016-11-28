86714842e47e67a5ce6e40a1da4dad284033e3a5
Aaron Watters <awatters@simonsfoundation.org>
2016-11-28 10:17:47 -0500

    fix triangle orientation.

--- contourist/tetrahedral.py
+++ contourist/tetrahedral.py
@@ -169,6 +169,7 @@ class Grid3DContour(object):
         for triangle in self.triangle_pair_triples:
             index_set = frozenset(pair_number[pair] for pair in triangle)
             set_of_triangles_indices.add(index_set)
+        (list_of_points, set_of_triangles_indices) = clean_triangles(list_of_points, set_of_triangles_indices)
         set_of_triangles_indices = orient_triangles(list_of_points, set_of_triangles_indices)
         return (list_of_points, set_of_triangles_indices)
 
@@ -288,6 +289,38 @@ class Grid3DContour(object):
             return ((low_point, high_point), interpolated)
         return interpolated
 
+def clean_triangles(list_of_points, set_of_triangle_indices):
+    keep_triangles = set()
+    keep_vertices = []
+    vertex_map = {}
+    def new_vertex_index(vertex_index):
+        if vertex_index in vertex_map:
+            return vertex_map[vertex_index]
+        else:
+            vertex = list_of_points[vertex_index]
+            result = len(keep_vertices)
+            vertex_map[vertex_index] = result
+            keep_vertices.append(vertex)
+            return result
+    for triangle in set_of_triangle_indices:
+        orientation = list(triangle)
+        [A, B, C] = [list_of_points[i] for i in orientation]
+        cross = np.cross(A - C, B - C)
+        if np.allclose(cross, 0):
+            #print "  omitting triangle", orientation
+            [a, b, c] = orientation
+            # empty triangle: omit triangle and merge identical vertices.
+            for (i, j) in [(a, b), (a, c), (b, c)]:
+                if np.allclose(list_of_points[i], list_of_points[j]):
+                    # merge vertices
+                    #print "      merging vertices", i, j
+                    merged = new_vertex_index(i)  # == vertex_map[i]
+                    vertex_map[j] = merged
+        else:
+            new_triangle = frozenset(new_vertex_index(i) for i in orientation)
+            keep_triangles.add(new_triangle)
+    return (keep_vertices, keep_triangles)
+
 def orient_triangles_kiss(list_of_points, set_of_triangle_indices):
     # make sure first component of cross product is positive
     oriented_triangles = set()
@@ -295,17 +328,95 @@ def orient_triangles_kiss(list_of_points, set_of_triangle_indices):
         orientation = tuple(triangle)
         (a, b, c) = [list_of_points[i] for i in orientation]
         if np.allclose(a, b) or np.allclose(b, c) or np.allclose(c, a):
-            print "omit", orientation, a, b, c
+            #print "omit", orientation, a, b, c
             pass  # ignore this triangle (and create a hole in the surface?)
         else:
             dotx = np.cross(b - a, c - a)[0]
             if dotx < 0:
-                print "reverse", orientation
+                #print "reverse", orientation
                 orientation = tuple(reversed(orientation))
             oriented_triangles.add(orientation)
     return oriented_triangles
 
-def orient_triangles(list_of_points, set_of_triangles_indices):
+def orient_triangles(list_of_points, set_of_triangle_indices):
+    #print "orienting", len(set_of_triangle_indices), "triangles"
+    segments_to_triangles = {}
+    triangle_orientations = {}
+    points_to_triangles_indices = {}
+    for triangle in set_of_triangle_indices:
+        for i in triangle:
+            points_to_triangles_indices.setdefault(i, set()).add(triangle)
+        (a, b, c) = triangle
+        for edge in ((a,b), (b,c), (a,c)):
+            s_edge = frozenset(edge)
+            segments_to_triangles.setdefault(s_edge, set()).add(triangle)
+    unoriented_triangles = set(set_of_triangle_indices)
+    while unoriented_triangles:
+        #print "unoriented", len(unoriented_triangles)
+        #vertex_indices = set(p for p in triangle for triangle in unoriented_triangles)
+        vertex_indices = set()
+        for triangle in unoriented_triangles:
+            for p in triangle:
+                vertex_indices.add(p)
+        # find a triangle with max x value for some point with non-zero cross product x component
+        (max_x, max_index) = max((list_of_points[i][0], i) for i in vertex_indices)
+        max_x_triangles = set(triangle for triangle in points_to_triangles_indices[max_index] if triangle in unoriented_triangles)
+        #if len(unoriented_triangles) < 10:
+            #print "unoriented", unoriented_triangles
+            #print "vertices", vertex_indices
+            #print "max_x", max_x, max_index
+            #print points_to_triangles_indices[max_index]
+        #print "max_x_triangles", max_x_triangles
+        initial_triangle = None
+        maxdotx = 0
+        for triangle in max_x_triangles:
+            (a, b, c) = [list_of_points[i] for i in triangle]
+            dotx = np.cross(a - b, a - c)[0]
+            if abs(dotx) >= abs(maxdotx):
+                maxdotx = dotx
+                initial_triangle = triangle
+        #print "initial triangle", triangle
+        # orient the initial triangle
+        orientation = tuple(initial_triangle)
+        #next_triangles = set(orientation)
+        (a, b, c) = [list_of_points[i] for i in orientation]
+        dotx = np.cross(a - b, a - c)[0]
+        # make sure dotx is *positive* (???)
+        if dotx < 0:
+            orientation = tuple(reversed(orientation))
+        def same_orientation(orientation1, orientation2):
+            if orientation1 == orientation2:
+                return True
+            (a, b, c) = orientation1
+            return ((b, c, a) == orientation2) or ((c, a, b) == orientation2)
+        stack = [(initial_triangle, orientation)]
+        def orient_edge(index1, index2, from_triangle):
+            edge = frozenset((index1, index2))
+            triangles = segments_to_triangles[edge]
+            assert len(triangles) <= 2, repr(list(triangles))
+            for triangle in triangles:
+                if triangle != from_triangle:
+                    [index3] = triangle - edge
+                    orientation = (index1, index2, index3)
+                    if triangle in triangle_orientations:
+                        orientation1 = triangle_orientations[triangle]
+                        if not same_orientation(orientation1, orientation):
+                            print "bad orientation?", orientation1, orientation
+                    else:
+                        stack.append((triangle, orientation))
+        while stack:
+            (triangle, orientation) = stack.pop()
+            #Sprint "orientation", triangle, orientation
+            triangle_orientations[triangle] = orientation
+            if triangle in unoriented_triangles:
+                unoriented_triangles.remove(triangle)
+            [a, b, c] = orientation
+            orient_edge(c, b, triangle)
+            orient_edge(b, a,  triangle)
+            orient_edge(a, c, triangle)
+    return list(sorted(triangle_orientations.values()))
+
+def orient_triangles_bogus(list_of_points, set_of_triangles_indices):
     "Consistently orient triangles so that right-hand-rule normals point 'outwards'."
     oriented_triangles = set()
     unoriented_triangles = set(set_of_triangles_indices)
@@ -314,7 +425,7 @@ def orient_triangles(list_of_points, set_of_triangles_indices):
     for triangle in list(unoriented_triangles):
         (a, b, c) = [list_of_points[i] for i in triangle]
         if np.allclose(a, b) or np.allclose(b, c) or np.allclose(c, a):
-            print "removing", triangle
+            #print "removing", triangle
             unoriented_triangles.remove(triangle)
     #points_to_segments_indices = {}
     points_to_triangles_indices = {}
@@ -340,10 +451,12 @@ def orient_triangles(list_of_points, set_of_triangles_indices):
             if abs(dotx) >= abs(maxdotx):
                 maxdotx = dotx
                 initial_triangle = triangle
-        print "initial triangle", triangle
+        #print "initial triangle", triangle
         segment_orientation = {}
-        next_triangles = set()
+        #next_triangles = set()
         def add_orientation(orientation, triangle):
+            if type(triangle) is not frozenset:
+                triangle = frozenset(triangle)
             (a, b, c) = orientation
             for segment in [(a,b), (b,c), (c,a)]:
                 segment_orientation[frozenset(segment)] = segment
@@ -355,12 +468,14 @@ def orient_triangles(list_of_points, set_of_triangles_indices):
                         next_triangles.add(other_triangle)
         # orient the initial triangle
         orientation = tuple(initial_triangle)
+        #next_triangles = set(orientation)
         (a, b, c) = [list_of_points[i] for i in orientation]
         dotx = np.cross(a - b, a - c)[0]
         # make sure dotx is *positive* (???)
         if dotx < 0:
             orientation = tuple(reversed(orientation))
-        add_orientation(orientation, initial_triangle)
+        #add_orientation(orientation, initial_triangle)
+        next_triangles = set([orientation])
         # keep adding connected triangles, oriented consistently with the initial triangle
         while next_triangles:
             chosen_triangle = next_triangles.pop()
@@ -371,7 +486,7 @@ def orient_triangles(list_of_points, set_of_triangles_indices):
                 if segment_orientation.get(frozenset(segment)) == segment:
                     reverse = True
             if reverse:
-                print "    reversing", orientation
+                #print "    reversing", orientation
                 orientation = tuple(reversed(orientation))
             add_orientation(orientation, chosen_triangle)
     return oriented_triangles
