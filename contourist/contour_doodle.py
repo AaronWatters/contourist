4249b0d36533ddb9e94f02b0d5ff8475a1a7f718
Aaron Watters <awatters@simonsfoundation.org>
2017-02-17 08:52:05 -0500

    enable flattening in doodles

--- contourist/contour_doodle.py
+++ contourist/contour_doodle.py
@@ -8,14 +8,20 @@ import numpy as np
 class CDoodle(doodle3d.Doodle3D):
 
     def implicit_surface(self, mins, maxes, delta, f, v, color, 
-            kind="solid", opacity=None, interpolate=False):
-        I = tetrahedral.TriangulatedIsosurfaces(mins, maxes, delta, f, v, [], linear_interpolate=interpolate)
+            kind="solid", opacity=None, interpolate=False,
+            flatten=False, minimum_ratio=None, minimum_extent=None):
+        I = tetrahedral.TriangulatedIsosurfaces(mins, maxes, delta, f, v, [], 
+                linear_interpolate=interpolate, flatten=flatten, minimum_extent=minimum_extent,
+                minimum_ratio=minimum_ratio)
         I.search_for_endpoints()
         (points, triangles) = I.get_points_and_triangles()
         return self.triangle_surface(points, triangles, color, kind, opacity)
 
-    def implicit_morph(self, mins, maxes, delta, f, v, duration, color, interpolate=True):
-        C = pentatopes.MorphingIsoSurfaces(mins, maxes, delta, f, v, [], linear_interpolate=interpolate)
+    def implicit_morph(self, mins, maxes, delta, f, v, duration, color, 
+        interpolate=True, flatten=False, minimum_ratio=None, minimum_extent=None):
+        C = pentatopes.MorphingIsoSurfaces(mins, maxes, delta, f, v, [],
+            linear_interpolate=interpolate, minimum_extent=minimum_extent, flatten=flatten,
+            minimum_ratio=minimum_ratio)
         C.search_for_endpoints()
         M = C.collect_morph_triangles()
         points = M.points4d
